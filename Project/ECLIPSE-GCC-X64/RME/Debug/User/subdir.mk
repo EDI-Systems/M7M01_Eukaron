ba785607ef791cc7bb29d82333a7d5e4656f6530
EDI Systems <644580703@qq.com>
2018-05-09 21:53:09 -0400

    :green_heart:travis-ci integration

--- Project/ECLIPSE-GCC-X64/RME/Debug/User/subdir.mk
+++ Project/ECLIPSE-GCC-X64/RME/Debug/User/subdir.mk
@@ -4,7 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/media/pry/Code/Code_Library/MCU/Mutatus/M7M3_MuRex/Project/ECLIPSE-GCC-X64/UVM/Debug/UVM.c 
+../UVM.c 
 
 OBJS += \
 ./User/UVM.o 
@@ -14,7 +14,7 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-User/UVM.o: /media/pry/Code/Code_Library/MCU/Mutatus/M7M3_MuRex/Project/ECLIPSE-GCC-X64/UVM/Debug/UVM.c
+User/UVM.o: /media/pry/Code/Code_Library/MCU/Mutatus/M7M1_MuEukaron/Project/ECLIPSE-GCC-X64/RME/UVM.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
 	gcc -m64 -mcmodel=kernel -nostdlib -I/media/pry/Code/Code_Library/MCU/Mutatus/M7M1_MuEukaron/Project/ECLIPSE-GCC-X64/RME/../../../MEukaron/Include -O3 -mno-sse -g3 -Wall -c -fmessage-length=0 -fno-pic -static -fno-builtin -fno-strict-aliasing -ffreestanding -fno-common -fno-stack-protector -mtls-direct-seg-refs -mno-red-zone -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
