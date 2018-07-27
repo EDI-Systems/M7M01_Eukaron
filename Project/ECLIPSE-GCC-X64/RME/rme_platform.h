19e6a00e78ab0f1c8c7488a390dc556fe770db7a
EDI Systems <644580703@qq.com>
2018-07-27 15:54:18 -0400

    :green_heart:fix x64 port

---
+++ Project/ECLIPSE-GCC-X64/RME/rme_platform.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+Filename    : rme_platform.h
+Author      : pry 
+Date        : 11/10/2017
+Licence     : LGPL v3+; see COPYING for details.
+Description : The platform specific types for RME.
+******************************************************************************/
+
+/* Platform Includes *********************************************************/
+#include "Platform/X64/rme_platform_x64.h"
+/* End Platform Includes *****************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
---
+++ Project/ECLIPSE-GCC-X64/RME/rme_platform_x64_conf.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+Filename   : rme_platform_x64_conf.h
+Author     : pry
+Date       : 24/06/2017
+Licence    : LGPL v3+; see COPYING for details.
+Description: The configuration file for x64 profile settings.
+******************************************************************************/
+
+/* Config Includes ***********************************************************/
+#include "Platform/X64/Profiles/Super/rme_platform_X64_SUPER.h"
+/* End Config Includes *******************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
