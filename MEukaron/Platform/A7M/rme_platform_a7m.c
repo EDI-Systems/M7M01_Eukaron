f53f20a48b042ef031def615b910b34754e378d6
EDI Systems <644580703@qq.com>
2019-10-28 17:58:30 -0400

    :arrow_up:small fix

--- MEukaron/Platform/A7M/rme_platform_a7m.c
+++ MEukaron/Platform/A7M/rme_platform_a7m.c
@@ -652,13 +652,13 @@ rme_ptr_t __RME_Low_Level_Init(void)
     RME_A7M_SCB_AIRCR=Temp;
     
     /* Set the priority of timer and faults to the lowest, SVC is slightly higher.
-     * If not, ARMv7-M may pend the SVC, thus causing the following execution:
+     * If not, ARMv7-M may pend the asynchronous SVC, causing the following execution:
      * 1> SVC is triggered but not yet in execution.
      * 2> SysTick or some other random vector comes in.
      * 3> Processor choose to respond to SysTick first.
      * 4> There is a context switch in SysTick, we switch to another thread.
      * 5> SVC is now processed on the new thread rather than the calling thread.
-     * This MUST be avoided, thus we place SVC at a higher priority?
+     * This MUST be avoided, thus we place SVC at a higher priority.
      * All vectors that have a higher priority than SVC shall not call any RME
      * kernel functions; for those who can call kernel functions, they must be
      * placed at a priority smaller than 0x80 and the user needs to guarantee
@@ -667,8 +667,8 @@ rme_ptr_t __RME_Low_Level_Init(void)
     __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_SVCALL, 0x80);
     __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_PENDSV, 0xFF);
     __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_SYSTICK, 0xFF);
-    __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_BUSFAULT, 0xFF);
-    __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_USAGEFAULT, 0xFF);
+    __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_BUSFAULT, 0x40);
+    __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_USAGEFAULT, 0x40);
     __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_DEBUGMONITOR, 0xFF);
     __RME_A7M_NVIC_Set_Prio(RME_A7M_IRQN_MEMORYMANAGEMENT, 0xFF);
 
