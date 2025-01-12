9f15468815b8a34b69e5a55a79b4271cab7e6e5e
黄隽文240108 <2769530151@xiaomi.com>
2025-01-12 23:39:56 +0800

    2025.1.12 build project

---
+++ Project/MFGCC-XC7Z020/rme_platform.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+Filename    : rme_platform.h
+Author      : pry 
+Date        : 22/07/2017
+Licence     : LGPL v3+; see COPYING for details.
+Description : The platform specific types for RME.
+******************************************************************************/
+
+/* Platform Include **********************************************************/
+#include "Platform/A7A/rme_platform_a7a.h"
+/* End Platform Include ******************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
---
+++ Project/MFGCC-XC7Z020/rme_platform_a7a_conf.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+Filename    : rme_platform_a7a_conf.h
+Author      : pry
+Date        : 24/06/2017
+Licence     : LGPL v3+; see COPYING for details.
+Description : The configuration file for Cortex-A HAL.
+******************************************************************************/
+
+/* Config Include ************************************************************/
+#include "Platform/A7A/Chip/XC7Z020/rme_platform_xc7z020.h"
+/* End Config Include ********************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
